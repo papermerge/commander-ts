950e3517bdbadf80c5ad349a087464ca1cc30af9
Eugen Ciur <eugen@papermerge.com>
2023-02-20 09:02:18 +0100

    with loadable breadcrumb

--- app/components/breadcrumb/index.ts
+++ app/components/breadcrumb/index.ts
@@ -1,28 +1,62 @@
-import Component from '@glimmer/component';
 import { trackedFunction } from 'ember-resources/util/function';
+import Component from '@glimmer/component';
+import { resource, use, resourceFactory } from 'ember-resources';
+import { TrackedObject } from 'tracked-built-ins';
+
 
 interface Args {
   endpoint: string;
   onClick: (item: string) => void;
 }
 
-export default class Breadcrumb extends Component<Args> {
-  data = trackedFunction(this, async () => {
-    const response = await fetch(
-      `http://127.0.0.1:8000/api/folders/${this.args.endpoint}/`,
-      {
+const NodesResource = resourceFactory((args_foo) => {
+
+  return resource(({ on }) => {
+
+    let [base_url, current_endpoint] = args_foo();
+
+    let state = new TrackedObject({
+      isResolved: false,
+      isLoading: true,
+      isError:
+      false,
+      value: null,
+      error: null
+    });
+
+    let controller = new AbortController();
+
+    on.cleanup(() => controller.abort());
+
+    fetch(`${base_url}${current_endpoint}/`,{
+        signal: controller.signal,
         headers: {
           Authorization:
             'Token 0c724ad3d4101ba0b602c7fff44f4ff60c39e07d533c7eb7175c1b6d2efb47e3',
         },
-      }
-    );
-    const json = await response.json();
+    })
+    .then(response => response.json())
+    .then(result => {
+      state.value = result.data ?? [];
+      state.isResolved = true;
+      state.isError = false;
+      state.isLoading = false;
+      console.log(state.value);
+    })
+    .catch(error => {
+      state.error = error;
+      state.isResolved = true;
+      state.isError = true;
+      state.isLoading = false;
+    });
 
-    return json.data;
+    return state;
   });
+});
 
-  get nodes() {
-    return this.data.value ?? [];
-  }
+
+export default class Breadcrumb extends Component<Args> {
+ @use load = NodesResource(() => [
+    `http://127.0.0.1:8000/api/folders/`, this.args.endpoint
+  ]);
 }
