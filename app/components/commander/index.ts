2b34bc5889baa23315f8a9bcf5304f1b0087298a
Eugen Ciur <eugen@papermerge.com>
2023-02-20 07:33:00 +0100

    better resource handling

--- app/components/commander/index.ts
+++ app/components/commander/index.ts
@@ -1,5 +1,8 @@
 import Component from '@glimmer/component';
-import { trackedFunction } from 'ember-resources/util/function';
+import { tracked } from '@glimmer/tracking';
+import { resource, use, resourceFactory } from 'ember-resources';
+import { TrackedObject } from 'tracked-built-ins';
+
 
 enum BaseTreeNodeType {
   folder = 'folders',
@@ -41,24 +44,60 @@ class BaseTreeNode implements IBaseTreeNode {
   }
 }
 
-export default class Commander extends Component<Args> {
-  data = trackedFunction(this, async () => {
-    const response = await fetch(
-      `http://127.0.0.1:8000/api/nodes/${this.args.endpoint}/`,
-      {
+const NodesResource = resourceFactory((endpoint) => {
+
+  return resource(({ on }) => {
+
+    let current_endpoint = endpoint;
+
+    if (typeof endpoint === 'function') {
+      current_endpoint = endpoint();
+    }
+
+    let state = new TrackedObject({
+      isResolved: false,
+      isLoading: true,
+      isError:
+      false,
+      value: null,
+      error: null
+    });
+
+    let controller = new AbortController();
+
+    console.log(endpoint);
+
+    on.cleanup(() => controller.abort());
+
+    fetch(`http://127.0.0.1:8000/api/nodes/${current_endpoint}/`,{
+        signal: controller.signal,
         headers: {
           Authorization:
             'Token 0c724ad3d4101ba0b602c7fff44f4ff60c39e07d533c7eb7175c1b6d2efb47e3',
         },
-      }
-    );
-    const json = await response.json();
-    return json.data;
+    })
+    .then(response => response.json())
+    .then(result => {
+      let items = result.data ?? [];
+      state.value = items.map((item: IBaseTreeNode) => new BaseTreeNode(item));
+      state.isResolved = true;
+      state.isError = false;
+      state.isLoading = false;
+    })
+    .catch(error => {
+      state.error = error;
+      state.isResolved = true;
+      state.isError = true;
+      state.isLoading = false;
+    });
+
+    return state;
   });
+});
 
-  get nodes(): BaseTreeNode[] {
-    const items = this.data.value ?? [];
 
-    return items.map((item: IBaseTreeNode) => new BaseTreeNode(item));
-  }
+export default class Commander extends Component<Args> {
+
+  @use load = NodesResource(() => this.args.endpoint);
+
 }
